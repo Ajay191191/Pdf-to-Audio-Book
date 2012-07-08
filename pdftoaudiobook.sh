74d85e29388d3574108e2ca35b365b94e9033027
Ajay <ajay191191@gmail.com>
2012-07-08 18:52:28 +0530

    Clean

--- pdftoaudiobook.sh
+++ pdftoaudiobook.sh
@@ -25,8 +25,6 @@ mkdir data-$1
 pdfsam-console -f $1.pdf -s BLEVEL -bl 1  -o data-$1/ split 1>asd.txt
 error_str=`cat asd.txt | grep FATAL`
 if [ -z "$error_str" ]; then
-echo "Successfuly split at chapters"
-echo "Splitting individual Chapters..."
 cd data-$1
 chapters=1
 renameDir="Cover"
@@ -34,14 +32,12 @@ for d in *.pdf
 do
 	arr2=($d)
 	mkdir ${arr2[0]}
-	echo ${arr2[0]}
 	pdfsam-console -f ${arr2[0]}.pdf -s BURST -o ${arr2[0]}/ split 1>/dev/null
 	cd ${arr2[0]}
 	first=1
 	for e in *.pdf
 	do
 		arr3=($e)
-		echo ${arr3[0]}
 		pdftoppm ${arr3[0]}.pdf > ${arr3[0]}.ppm
 		mogrify -scale 200% ${arr3[0]}.ppm
 		tesseract ${arr3[0]}.ppm ${arr3[0]} 1>/dev/null 2>/dev/null
@@ -57,18 +53,16 @@ do
 	sox *.wav $renameDir.wav 1>/dev/null 2>/dev/null
 	gst-launch-0.10 filesrc location=$renameDir.wav ! decodebin ! lame ! filesink location=$renameDir.mp3 1>/dev/null 2>/dev/null
 	cd ..
-	mv ${arr2[0]} $renameDir
+	mv ${arr2[0]} ${renameDir//[[:space:]]}
 done
 rm *.pdf
 else
-echo "Error No Bookmarks found.\n Splitting page by page"
 pdfsam-console -f $1.pdf -s BURST -o data-$1/ split 1>/dev/null
 cd data-$1
 for c in *.pdf
 do
 	arr1=($c)
 	string1=${arr1[0]}
-	echo $string1
 	pdftoppm $string1.pdf > $string1.ppm 
 	mogrify -scale 200% $string1.ppm
 	tesseract $string1.ppm $string1 1>/dev/null 2>/dev/null
@@ -77,5 +71,3 @@ done
 sox *.wav $string.wav 1>/dev/null 2>/dev/null
 gst-launch-0.10 filesrc location=$string.wav ! decodebin ! lame ! filesink location=$string.mp3 1>/dev/null 2>/dev/null
 fi
-
-
